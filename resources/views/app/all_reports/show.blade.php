898761c09ac3bbda7b39371210075d80814a8d7d
Abraham Sisay <adem.abraham@ju.edu.et>
2023-04-20 01:11:57 -0700

    date picker added

--- resources/views/app/all_reports/show.blade.php
+++ resources/views/app/all_reports/show.blade.php
@@ -17,117 +17,130 @@
                 <div class="card-body">
                     <h4 class="card-title w-100 mb-2">Report</h4>
                     <div class="mb-4">
-                      <h5>@lang('crud.user_supports.inputs.user_id')</h5>
-                      <span
-                          >{{ optional($reports->user)->full_name ?? '-'
-                          }}</span
-                      >
-                  </div>
-                  </div>
-
-
-
+                        <h5>@lang('crud.user_supports.inputs.user_id')</h5>
+                        <span>{{ optional($reports->user)->full_name ?? '-' }}</span>
+                    </div>
                 </div>
+
+
+
             </div>
-        @endcan
-        <div class="container-fluid">
-            <h5 class="mb-2">Info Box</h5>
-            <div class="row">
-                <div class="col-md-3 col-sm-6 col-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+        </div>
+    @endcan
+    <div class="container-fluid">
+        <h5 class="mb-2">Info Box</h5>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
 
-                        <div class="info-box-content">
-                            <span class="info-box-text">Total Tickets </span>
-                            <span class="info-box-number">{{ $totalTicket }}</span>
-                        </div>
-                        <!-- /.info-box-content -->
+                    <div class="info-box-content">
+                        <span class="info-box-text">Total Tickets </span>
+                        <span class="info-box-number">{{ $totalTicket }}</span>
                     </div>
-                    <!-- /.info-box -->
+                    <!-- /.info-box-content -->
                 </div>
-                <!-- /.col -->
-                <div class="col-md-3 col-sm-6 col-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Active Tickets</span>
-                            <span class="info-box-number">{{ $totalactiveTicket }}</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-md-3 col-sm-6 col-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Escaleted Tickets</span>
-                            <span class="info-box-number">{{ $todaysTicket }}</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-md-3 col-sm-6 col-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Closed TIckets</span>
-                            <span class="info-box-number">{{ $totalClosedTicket }}</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
+                <!-- /.info-box -->
             </div>
-            <!-- /.row -->
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
 
+                    <div class="info-box-content">
+                        <span class="info-box-text">Active Tickets</span>
+                        <span class="info-box-number">{{ $totalactiveTicket }}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
 
+                    <div class="info-box-content">
+                        <span class="info-box-text">Escaleted Tickets</span>
+                        <span class="info-box-number">{{ $todaysTicket }}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
 
-            <script>
-                $(function() {
-                    let today = new Date();
-                    let year = today.getFullYear();
-                    let month = (today.getMonth() + 1).toString().padStart(2, '0');
-                    let day = today.getDate().toString().padStart(2, '0');
-                    let formattedDate = `${year}.${month}.${day}`;
-                    //console.log(formattedDate);
-                    $("#slider-range").slider({
-                        range: true,
-                        min: new Date('2023.01.01').getTime() / 1000,
-                        max: new Date(formattedDate).getTime() / 1000,
-                        step: 86400,
-                        // values: [new Date('2013.01.01').getTime() / 1000, new Date('2013.02.01').getTime() / 1000],
-                        slide: function(event, ui) {
-                            $("#amount").val((new Date(ui.values[0] * 1000).toDateString()) + " - " + (new Date(
-                                ui.values[1] * 1000)).toDateString());
-                        }
-                    });
-                    $("#amount").val((new Date($("#slider-range").slider("values", 0) * 1000).toDateString()) +
-                        " - " + (new Date($("#slider-range").slider("values", 1) * 1000)).toDateString());
-                });
-            </script>
-
-            <p>
-                <label for="amount">Date range:</label>
-                <input type="text" id="amount" style="border: 0; color: #f6931f; font-weight: bold;" size="100" />
-            </p>
-            <div id="slider-range"></div>
-
-
-
-
-
-
+                    <div class="info-box-content">
+                        <span class="info-box-text">Closed TIckets</span>
+                        <span class="info-box-number">{{ $totalClosedTicket }}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
             <!-- /.col -->
         </div>
         <!-- /.row -->
+
+
+
+        <script type="text/javascript">
+            $(function() {
+
+                var start = moment().subtract(29, 'days');
+                var end = moment();
+
+                function cb(start, end) {
+                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                }
+
+                $('#reportrange').daterangepicker({
+                    startDate: start,
+                    endDate: end,
+                    ranges: {
+                        'Today': [moment(), moment()],
+                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                        'This Month': [moment().startOf('month'), moment().endOf('month')],
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
+                            'month').endOf('month')],
+                        'This Year': [moment().startOf('year'), moment().endOf('year')]
+                    }
+                }, cb);
+
+                cb(start, end);
+
+            });
+        </script>
+
+        <div id="reportrange" class="pull-left col-6"
+            style="background: #fff; cursor: pointer; padding: 10px 20px; border: 1px solid #ccc; width: 100%">
+            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+            <span id="dateRange"></span> <b class="caret"></b>
+
+            <script>
+                var dateRange = document.getElementById('dateRange');
+                
+
+            </script>
+            <input type="hidden" id="" 
+            
+            
+            />
+        </div>
+
+
+
+
+
+
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
 
 
 
