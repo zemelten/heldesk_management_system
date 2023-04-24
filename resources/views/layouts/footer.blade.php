3c9d92f742069187278e7f308d54969deaf58672
ztenaw <zelalem.abrew@ju.edu.et>
2023-04-24 16:23:15 +0300

    merge into test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <strong>Copyright &copy; <script>
         document.write(new Date().getFullYear());
-    </script> <a href="ju.edu.et">Jimma University</a>.</strong>
+    </script> <a href="https://www.ju.edu.et">Jimma University</a>.</strong>
     All rights reserved. Designed By JU-ICT
     <div class="float-right d-none d-sm-inline-block">
      
